a05e90380e9df6f5fcf160d7093d7fefc94b80be
dantan2000 <tan.da@husky.neu.edu>
2022-04-21 17:05:49 -0400

    Added comments to service methods

--- src/services/scorecard-services.js
+++ src/services/scorecard-services.js
@@ -2,31 +2,38 @@ import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const SCORECARDS_API = `${API_BASE}/scorecards`;
 
+// Creates a scorecard in the database
 export const createScorecard = async(newScorecard) => {
   const response = await axios.post(SCORECARDS_API, newScorecard)
   return response.data;
 }
 
+// Finds all public scorecards
 export const findAllScorecards = async() => {
   const response = await axios.get(SCORECARDS_API);
   const scorecards = response.data;
   return scorecards;
 }
 
+// Finds all scorecards made by a user given their username
+// Automatically checks the browser's cookies to determine whether to show
+// only public or all scorecards
 export const findScorecardsByUsername = async(username) => {
   const response = await axios.get(`${SCORECARDS_API}/${username}`);
   const scorecards = response.data;
   return scorecards;
 }
 
-export const deleteScorecard = async(tuit) => {
+// Deletes a given scorecard
+export const deleteScorecard = async(scorecard) => {
   const response = await axios
-    .delete(`${SCORECARDS_API}/${tuit._id}`);
+    .delete(`${SCORECARDS_API}/${scorecard._id}`);
   return response.data;
 }
 
-export const updateScorecard = async(tuit) => {
+// Updates a given scorecard
+export const updateScorecard = async(scorecard) => {
   const response = await axios
-    .put(`${SCORECARDS_API}/${tuit._id}`, tuit);
+    .put(`${SCORECARDS_API}/${scorecard._id}`, scorecard);
   return response.data;
 }
--- src/services/user-services.js
+++ src/services/user-services.js
@@ -6,26 +6,29 @@ const USERS_API = `${API_BASE}/users`;
 // If successful, automatically sets browser cookies
 export const createUser = async(newUser) => {
   try {
-    const response = await axios.post(USERS_API, newUser)
+    const response = await axios.post(USERS_API, newUser);
     return true;
   } catch (err) {
-    return false
+    return false;
   }
 }
 
-
-export const findAllScorecards = async() => {
+// Finds all users in the database
+export const findAllUsers = async() => {
   const response = await axios.get(USERS_API);
-  const scorecards = response.data;
-  return scorecards;
+  const users = response.data;
+  return users;
 }
 
+// Given a username, finds the user in the database with that username
 export const findUserByUsername = async(username) => {
   const response = await axios.get(`${API_BASE}/user/${username}`);
   const user = response.data;
   return user;
 }
 
+// Finds the user based on the browser's cookies
+// Returns undefined if no user was found
 export const findUserByCookie = async() => {
   try {
     const response = await axios.get(`${API_BASE}/user_by_cookie`);
@@ -36,12 +39,16 @@ export const findUserByCookie = async() => {
   }
 }
 
+// Deletes a user from the database
+// Takes in a full User type
 export const deleteUser = async(user) => {
   const response = await axios
     .delete(`${USERS_API}/${user._id}`);
   return response.data;
 }
 
+// Updates a user in the database
+// Takes in a full User type
 export const updateUser = async(user) => {
   const response = await axios
     .put(`${USERS_API}/${user._id}`, user);
@@ -53,7 +60,7 @@ export const updateUser = async(user) => {
 // If successful, will automatically set browser cookies
 export const loginUser = async(username, password) => {
   try {
-    const response = await axios.put(`${API_BASE}/user_login`);
+    const response = await axios.put(`${API_BASE}/user_login`, {username: username, password: password});
     return true;
   } catch (err) {
     return false;
