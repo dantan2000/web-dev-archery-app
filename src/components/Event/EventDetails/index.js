693b43ae83cace6d1ff368b61794e6a8969d5304
dantan2000 <tan.da@husky.neu.edu>
2022-04-23 14:59:50 -0400

    Event resources loading from backend

--- src/components/Event/EventDetails/index.js
+++ src/components/Event/EventDetails/index.js
@@ -1,60 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { findEventByID } from '../../../services/world-archery-services';
 
 const EventDetails = () => {
-
+  const [error, setError] = useState(false)
+  const [event, setEvent] = useState(undefined);
   const { eid } = useParams();
 
-  try {
-    const event = await findEventByID(eid);
-    console.log(event);
-    const jsonString = JSON.stringify(event);
-    console.log(jsonString);
-    const str = 'hello world'
-    return (<div>{str}</div>);
-  } catch (err) {
-    console.log("Errored");
-    return (<div>{err.toString()}</div>);
+  useEffect(() => {
+    findEventByID(eid)
+      .then(response => setEvent(response))
+      .catch(() => setError(true));
+  })
+
+  if (error) {
+    return <div>Error: Something went wrong :(</div>
   }
-
-
-  return (
-    <>
-      <div className="row">
-        <div>
-          back button
-        </div>
-      </div>
-      <div className="row">
-        <div>
-          Event Details
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <span>
-            heart :) {/* get fontawesome heart and make it hearted based on smt */}
-            {event.Name}
-            live {/* go remember how to make an if statement in html stuff */}
-
-          </span>
+  if (event) {
+    return (
+      <>
+        <div className="row">
           <div>
-            FLAG
-          </div>
-          <div>
-            this is where the infor goes
+            back button
           </div>
         </div>
-        <div className="col">
-          List of people who favourited
+        <div className="row">
+          <div>
+            Event Details
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <span>
+              heart :) {/* get fontawesome heart and make it hearted based on smt */}
+              {event.Name}
+              live {/* go remember how to make an if statement in html stuff */}
+
+            </span>
+            <div>
+              FLAG
+            </div>
+            <div>
+              this is where the infor goes
+            </div>
+          </div>
+          <div className="col">
+            List of people who favourited
+          </div>
+
         </div>
 
-      </div>
-
-    </>
-
-  )
+      </>
+    );
+  }
+  return <div>loading...</div>
 }
 export default EventDetails;
