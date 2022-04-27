d9ec3f145ba8da507e063d7d2a00979ce23690c3
dantan2000 <tan.da@husky.neu.edu>
2022-04-27 15:33:38 -0400

    Created sign in form

--- frontend/src/components/LoginPage/SignIn.js
+++ frontend/src/components/LoginPage/SignIn.js
@@ -1,6 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import { useState, useContext } from 'react';
 
+
+import CurrUserContext from '../../contexts/CurrUserContext';
+import { loginUser } from '../../services/user-services';
 
 const SignIn = () => {
-  return <div>Sign In</div>;
+  const [awaitingResponse, setAwaitingResponse] = useState(false);
+  const {currUser, setCurrUser} = useContext(CurrUserContext);
+  const [errorMsg, setErrorMsg] = useState('');
+  const navigate = useNavigate();
+
+  if (currUser) {
+    navigate('/'); 
+  }
+
+  const login = () => {
+    if (!awaitingResponse) {
+      setErrorMsg('');
+      loginUser(document.getElementById('username'), document.getElementById('username'))
+        .then(response => {
+          setCurrUser(response);
+          navigate('/');
+        })
+        .catch(() => setErrorMsg('Invalid username/password'));
+    }
+  }
+
+  return <>
+    <div>Sign In</div>
+      <label for='username'>Username: </label>
+      <input type='text' name='username' id='username'/>
+      <label for='username'>Password: </label>
+      <input type='password' name='password' id='password'/>
+      {errorMsg !== '' && errorMsg}
+      <button onClick={login}>Log In</button>
+  </>;
 }
 export default SignIn;
