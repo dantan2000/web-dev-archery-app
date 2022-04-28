4df0de06d6930906cbae7b0ea821bc4c9a1ed836
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2022-04-28 13:36:42 -0400

    login styling

--- frontend/src/components/LoginPage/SignUp.js
+++ frontend/src/components/LoginPage/SignUp.js
@@ -1,19 +1,18 @@
 import { useNavigate } from 'react-router-dom';
 import { useState, useContext } from 'react';
 
-
 import CurrUserContext from '../../contexts/CurrUserContext';
 import { createUser } from '../../services/user-services';
 import PrivacyPolicy from '../PrivacyPolicy';
 
 const SignUp = () => {
   const [awaitingResponse, setAwaitingResponse] = useState(false);
-  const {currUser, setCurrUser} = useContext(CurrUserContext);
+  const { currUser, setCurrUser } = useContext(CurrUserContext);
   const [errorMsg, setErrorMsg] = useState('');
   const navigate = useNavigate();
 
   if (currUser) {
-    navigate('/'); 
+    navigate('/');
   }
 
   const signup = () => {
@@ -33,13 +32,13 @@ const SignUp = () => {
       setErrorMsg('');
       setAwaitingResponse(true);
       createUser({
-        username: document.getElementById('username').value, 
+        username: document.getElementById('username').value,
         password: document.getElementById('password').value,
         admin_requested: document.getElementById('admin_requested').checked
       }).then(response => {
-          setCurrUser(response);
-          navigate('/');
-        })
+        setCurrUser(response);
+        navigate('/');
+      })
         .catch(err => {
           if (err.response?.data.message) {
             setErrorMsg(err.response.data.message);
@@ -52,43 +51,55 @@ const SignUp = () => {
   }
 
   const submitOnEnter = (e) => {
-    if(e && e.keyCode == 13) {
+    if (e && e.keyCode == 13) {
       signup();
     }
   }
 
   return <>
-    <div>Sign Up</div>
-    <div onKeyDown={submitOnEnter}>
-      <label for='username'>Username: </label>
-      <input type='text' name='username' id='username'/>
-      <label for='username'>Password: </label>
-      <input type='password' name='password' id='password'/>
-      <label for='username'>Confirm Password: </label>
-      <input type='password' name='password' id='passwordConfirm'/>
-      
-      <div className='custom-control custom-switch'>
-        <input
-          type='checkbox'
-          className='custom-control-input'
-          id='admin_requested'
-          readOnly
-        />
-        <label className='custom-control-label' htmlFor='admin_requested'>
-          Apply to be an event admin
-        </label>
+    <div class="row">
+      <div class="text-right col">
+      <img
+          className="wd-login-logo"
+          src="/images/logo_noBG.png" />
       </div>
+      <div class="col">
+        <div onKeyDown={submitOnEnter}>
+          <label for='username'>Username:</label><br/>
+          <input type='text' name='username' id='username'/><br/>
+          <label for='username'>Password:</label><br/>
+          <input type='password' name='password' id='password'/><br/>
+          <label for='username'>Confirm Password:</label><br/>
+          <input type='password' name='password' id='passwordConfirm'/><br/>
 
-      {<PrivacyPolicy/>}
-      
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="privacyPolicyConsent"/>
-        <label class="form-check-label" for="privacyPolicyConsent">I have read and agree with the privacy policy above</label>
+          <div className='custom-control custom-switch'>
+            <input
+              type='checkbox'
+              className='custom-control-input'
+              id='admin_requested'
+              readOnly
+            />
+            <label className='custom-control-label' htmlFor='admin_requested'>
+              Apply to be an event admin
+            </label>
+          </div>
+
+          <div class="my-2">
+          {<PrivacyPolicy />}
+          </div>
+
+          <div class="my-2 form-check">
+            <input type="checkbox" class="form-check-input" id="privacyPolicyConsent" />
+            <label class="form-check-label" for="privacyPolicyConsent">I have read and agree with the privacy policy above</label>
+          </div>
+          <div>
+          {errorMsg !== '' && errorMsg}
+          </div>
+          <button class="btn-primary" onClick={signup} disabled={awaitingResponse}>Sign Up</button>
+        </div>
       </div>
-
-      {errorMsg !== '' && errorMsg}
-      <button onClick={signup} disabled={awaitingResponse}>Sign Up</button>
     </div>
+
   </>;
 }
 export default SignUp;
