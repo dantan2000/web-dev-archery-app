57e91d3fb2114ffce2523d492679e225d5ea30b9
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2022-04-28 16:22:32 -0400

    refactored sc doesn't work

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {findAllScorecards} from "../../services/scorecard-services"
+
 import EventList from "../Event/EventList";
 import ScorecardList from "../Scorecard/ScorecardList";
 
 const HomePage = () => {
+  const [scLoading, setSCLoading] = useState(true);
+  const [scError, setSCError] = useState(false)
+  const [scorecards, setScorecards] = useState([]);
+
+  useEffect(() => {
+    if (scorecards.length == 0 && !scError) {
+      findAllScorecards()
+        .then(response => setScorecards(response))
+        .catch(() => setSCError(true))
+        .finally(() => setSCLoading(false));
+    }
+  }, [])
   return (
     <>
       <div>Home Page</div>
       <div>
         <h2>Upcoming Events</h2>
-        <EventList/>
+        <EventList />
       </div>
       <div>
-      <h2>Featured Scorecards</h2>
-        <ScorecardList/>
+        <h2>Featured Scorecards</h2>
+        <ScorecardList
+          scorecards={scorecards}
+          scError={scError}
+          scLoading={scLoading}
+          showArcher="true"
+          showScore="false"
+          showNote="false" />
       </div>
 
     </>
